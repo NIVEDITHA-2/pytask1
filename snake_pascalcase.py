cf0839e284289512fcaaee9518f80ac9489a6de3
NIVEDITHA-2 <nivedithavava1@gmail.com>
2024-08-25 19:27:22 +0530

    first commit

---
+++ snake_pascalcase.py
@@ -0,0 +1,5 @@
+txt="gods_own_country"
+print("The original string:",txt)
+#convert snake to pascal
+x=txt.replace("_"," ").title().replace("","")#title() to convert lower case to upper case
+print("the string after converting snake to pascal case is:",x)# replace underscore with the space
